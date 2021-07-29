6434b027a899b8caa38e7c5a6844b11747f27c09
MansourWolou <Wolou0444@gmail.com>
2021-07-29 16:04:49 +0000

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 console.log(__dirname + "/views/index.html");
-
+app.use("/",function (req,res){
+ express.static(__dirname + "/public/style.css");
+})
 app.get("/",function(req, res) {
  res.sendFile(__dirname + "/views/index.html");
 })
