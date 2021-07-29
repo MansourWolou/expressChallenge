ef21b30aba37bb726d44bcf818726fb447e19d56
MansourWolou <Wolou0444@gmail.com>
2021-07-29 16:44:22 +0000

    Serve Static Assets fix

--- myApp.js
+++ myApp.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 
-console.log(__dirname + "/views/index.html");
-app.use("/",function (req,res){
- express.static(__dirname + "/public/style.css");
-})
+console.log(__dirname + "/public/style.css");
+app.use(express.static(__dirname + "/public"));
 app.get("/",function(req, res) {
  res.sendFile(__dirname + "/views/index.html");
 })
