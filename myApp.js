dcea4b5538da1caeb4ace29ebdb3a61fe35619f0
MansourWolou <Wolou0444@gmail.com>
2021-07-29 17:09:21 +0000

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -7,7 +7,9 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/",function(req, res) {
  res.sendFile(__dirname + "/views/index.html");
 })
-
+app.get("/json",function (req , res){
+ res.json({message : "hello json"});
+})
 
 
 
