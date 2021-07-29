b75ae984f30837a5ae47d894b2a294ad37a3c564
MansourWolou <Wolou0444@gmail.com>
2021-07-29 17:32:51 +0000

    Serve JSON on a Specific Route rebuild

--- myApp.js
+++ myApp.js
@@ -7,9 +7,11 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/",function(req, res) {
  res.sendFile(__dirname + "/views/index.html");
 })
-app.get("/json",function (req , res){
- res.json({message : "hello json"});
-})
+app.get("/json", (req, res) => {
+ res.json({
+  message: "Hello json"
+ });
+});
 
 
 
