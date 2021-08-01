5d38681ae2f8ff3e7755aefcef169c3faacf6255
MansourWolou <Wolou0444@gmail.com>
2021-08-01 22:45:43 +0000

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -4,13 +4,20 @@ var app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 app.use("/public", express.static(__dirname + "/public"));
-app.use( (req,res,next) =>{
-    console.log(req.method+" "+req.path+" - "+req.ip);
-    next();
-});
 app.get("/",function(req, res) {
  res.sendFile(__dirname + "/views/index.html");
 })
+app.get("/now",(req,res,next)=>{
+    req.time = new Date().toString()
+        next();
+},(req,res)=>{
+    res.json(
+        {
+            time: req.time
+        }
+    )
+})
+
 // don't know why it is not working.
 app.get("/", (req, res) => {
     var reponse;
