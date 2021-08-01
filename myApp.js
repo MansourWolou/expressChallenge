0b0eb2e36a15d86821315ff57375c6ff3194f69b
MansourWolou <Wolou0444@gmail.com>
2021-08-01 21:54:59 +0000

    Implement a Root-Level Request Logger Middlewareanother day

--- myApp.js
+++ myApp.js
@@ -3,11 +3,14 @@ const Process = require("process");
 var app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-//app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
-/*app.get("/",function(req, res) {
+app.use( (req,res,next) =>{
+    console.log(req.method+" "+req.path+" - "+req.ip);
+    next();
+});
+app.get("/",function(req, res) {
  res.sendFile(__dirname + "/views/index.html");
-})*/
+})
 // don't know why it is not working.
 app.get("/", (req, res) => {
     var reponse;
