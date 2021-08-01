eb631cbd28868dc7e669f766517f7d48ea189656
MansourWolou <Wolou0444@gmail.com>
2021-08-01 14:27:29 +0000

    fixing using freecodecamp forum help

--- myApp.js
+++ myApp.js
@@ -3,24 +3,20 @@ const Process = require("process");
 var app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-const  msg = Process.env.MESSAGE_STYLE;
-
-
-console.log( msg);
 //app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/",function(req, res) {
  res.sendFile(__dirname + "/views/index.html");
 })
-
-app.get("/json", (req, res) => {
-    let rslt;
-    if (msg === "uppercase"){
-      rslt = {message: "HELLO JSON"};
-     }else {
-      rslt = {message: "hello json"};
-     }
-res.json(rslt)
-
-});
+app.get("/", (req, res) => {
+    var reponse;
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        response = "Hello World".toUpperCase();
+    } else {
+        response = "Hello World";
+    }
+    res.json({
+        message: response
+    });
+})
 module.exports = app;
